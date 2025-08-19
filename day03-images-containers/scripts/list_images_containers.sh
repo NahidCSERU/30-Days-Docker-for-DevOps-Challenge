bba3ea14869e73619c36e95d3f96b8a737e74630
NahidCSERU <nahid.hasancseru@gmail.com>
2025-08-19 21:11:55 +0600

    feat(bash): Day 3 – status check of image and  container bash script

---
+++ day03-images-containers/scripts/list_images_containers.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Day-02: List Docker images and containers
+# Author: <Nahid Hasan>
+# Date: $(19/08/2025)
+echo "=== Docker Images ==="
+docker images
+
+echo -e "\n=== Running Containers ==="
+docker ps
